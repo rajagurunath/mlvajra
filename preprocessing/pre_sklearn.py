fac4ea01d71da6106aa893c3381d46e2bd8be62a
rajagurunath <rajagurunath5@gmail.com>
2019-02-22 22:38:02 +0530

    adding some extra files

---
+++ preprocessing/pre_sklearn.py
@@ -0,0 +1,61 @@
+
+from collections import namedtuple
+from sklearn.preprocessing import (Binarizer,FunctionTransformer,KBinsDiscretizer,
+                                    KernelCenterer,LabelBinarizer,LabelEncoder,
+                                    MultiLabelBinarizer,MaxAbsScaler, MinMaxScaler,
+                                    Normalizer,OneHotEncoder,OrdinalEncoder,
+                                    PolynomialFeatures,PowerTransformer,QuantileTransformer,
+                                    RobustScaler,StandardScaler,add_dummy_feature ,binarize,
+                                    label_binarize,maxabs_scale,minmax_scale,normalize,
+                                    quantile_transform,robust_scale,scale,power_transform)
+
+
+__all__=['Binarizer','FunctionTransformer','KBinsDiscretizer',
+            'KernelCenterer','LabelBinarizer','LabelEncoder',
+            'MultiLabelBinarizer','MaxAbsScaler', 'MinMaxScaler',
+            'Normalizer','OneHotEncoder','OrdinalEncoder',
+            'PolynomialFeatures','PowerTransformer','QuantileTransformer',
+            'RobustScaler','StandardScaler','add_dummy_feature' ,'binarize',
+            'label_binarize','maxabs_scale','minmax_scale','normalize',
+            'quantile_transform','robust_scale','scale','power_transform']
+
+
+tabular_transforms=['Binarizer','FunctionTransformer','KBinsDiscretizer',
+            'KernelCenterer','LabelBinarizer','LabelEncoder',
+            'MultiLabelBinarizer','MaxAbsScaler', 'MinMaxScaler',
+            'Normalizer','OneHotEncoder','OrdinalEncoder',
+            'PolynomialFeatures','PowerTransformer','QuantileTransformer',
+            'RobustScaler','StandardScaler','add_dummy_feature' ,'binarize',
+            'label_binarize','maxabs_scale','minmax_scale','normalize',
+            'quantile_transform','robust_scale','scale','power_transform']
+
+Tabular=namedtuple('Tabular',tabular_transforms)
+Tabular.Binarizer=Binarizer
+Tabular.FunctionTransformer=FunctionTransformer
+Tabular.KBinsDiscretizer=KBinsDiscretizer
+Tabular.KernelCenterer=KernelCenterer
+Tabular.LabelBinarizer=LabelBinarizer
+Tabular.LabelEncoder=LabelEncoder
+Tabular.MultiLabelBinarizer=MultiLabelBinarizer
+Tabular.MaxAbsScaler=MaxAbsScaler
+Tabular.MinMaxScaler=MinMaxScaler
+Tabular.Normalizer=Normalizer
+Tabular.OneHotEncoder=OneHotEncoder
+Tabular.OrdinalEncoder=OrdinalEncoder
+Tabular.PolynomialFeatures=PolynomialFeatures
+Tabular.PowerTransformer=PowerTransformer
+Tabular.QuantileTransformer=QuantileTransformer
+
+
+
+
+
+
+
+import torch
+
+
+
+
+
+
---
+++ preprocessing/skl.py
@@ -0,0 +1,5 @@
+from pre_sklearn import Tabular
+
+Tabular.add_dummy_feature
+Tabular.FunctionTransformer()
+Tabular.add_dummy_feature
